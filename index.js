7d97ec6593b31b330c22f36d63ab2484bc185b00
BoltUltra <75336728+BoltUltra@users.noreply.github.com>
2022-03-02 13:01:04 +0000

    Adding functionalities

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 10;
 let secondCard = 5;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
@@ -14,7 +15,7 @@ function startGame() {
 }
 
 function renderGame() {
-  cardsEl.textContent = `Cards: ${firstCard}  ${secondCard}`;
+  cardsEl.textContent = `Cards: ${cards[0]}  ${cards[1]}`;
   sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
@@ -31,6 +32,7 @@ function renderGame() {
 function newCard() {
   let card = 5;
   sum += card;
-
+  // Push the card to the cards array
+  cards.push(card);
   renderGame();
 }
